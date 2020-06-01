1b3dbe340d698aa386e1c98ba6964486294b4dac
yilu-zhang <861280152@qq.com>
2020-06-01 21:30:53 +0800

    show encoder

---
+++ src/rasp4b_core/src/show_encoder_vel.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "rasp4b_core/SensorState.h"
+
+double last_t=0;
+int wrong_cnt=0;
+void show_vel_callback(const rasp4b_core::SensorState::ConstPtr& msg)
+{
+	double now_t = msg->header.stamp.toSec();
+	if(now_t - last_t > 0.0021)
+	{
+		wrong_cnt++;		
+		ROS_WARN("wrong_seq:%d %d\n\n",msg->header.seq,wrong_cnt);	
+	}
+	last_t = now_t;
+	printf("%f:%f\n",msg->header.stamp.toSec(),(msg->r_velocity+msg->l_velocity)/2);
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc,argv,"show_encoder_vel");
+	printf("This is show_encoder_vel\n");
+	ros::NodeHandle n;
+
+	ros::Subscriber vel_sub = n.subscribe("/loui_robot1/sensor_state",10000,show_vel_callback);
+	ros::spin();
+	return 0;
+
+}
